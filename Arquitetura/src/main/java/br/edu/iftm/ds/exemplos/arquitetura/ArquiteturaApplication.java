728e5dbe6ea0ee488be6c40194cfe08c71539ea8
turattis <isabelaturattis@gmail.com>
2024-04-03 23:27:12 -0300

    trabalho

---
+++ Arquitetura/src/main/java/br/edu/iftm/ds/exemplos/arquitetura/ArquiteturaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exemplos.arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaApplication.class, args);
+	}
+
+}
---
+++ Arquitetura/src/test/java/br/edu/iftm/ds/exemplos/arquitetura/ArquiteturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.exemplos.arquitetura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
